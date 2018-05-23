a2cc069334043784144a504c9de2347a7bfad46b
azispc <aziscs20@gmail.com>
2018-05-23 10:59:59 +0700

    23_05_2018

---
+++ xubuntu.sh
@@ -0,0 +1,9 @@
+##membuat tulisan  dan memasukkan dalam file format
+echo "tulisan" > namafile.sh
+
+##copy,cut, delete, memindahkan file folder 1 ke folder 2
+cp readme.sh Desktop/labkom1 Download/labkom2
+mv readme.sh Desktop/labkom1 Download/labkom2
+rm readme.sh Desktop/labkom1
+ 
+
