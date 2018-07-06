4739a2ae8fbd2e573b8b6b707ab238ef1806706a
azispc <aziscs20@gmail.com>
2018-07-06 10:32:32 +0700

    Update xubuntu.sh

--- xubuntu.sh
+++ xubuntu.sh
@@ -9,3 +9,16 @@ rm readme.sh Desktop/labkom1
 ##crop photo 
 sudo apt install shotwell 
 
+#Cara mudah cek Processor, Memory, Harddisk, VGA, dll on Linux
+#Untuk sistem operasi Linux dapat menggunakan beberapa perintah dibawah ini:
+cat /proc/cpuinfo | Informasi Processor
+cat /proc/meminfo | Informasi Memory
+lshw | Untuk list perangkat keras
+fdisk -l | Informasi Harddisk dan partisi
+lspci | Informasi Controller, PCI, dll
+lspci | grep VGA | Informasi VGA / Display Card
+lspci | grep Eth | Informasi Ethernet Card / NIC / LAN Card
+uname -a | Informasi Kernel dan jenis Processor
+#eksekusi perintah dapat dilakukan di terminal/konsol, dan harus sebagai root, jika menggunakan Ubuntu dapat ditambahkan perintah sudo untuk mempermudah, mis:
+sudo lshw
+sudo cat /proc/cpuinfo
